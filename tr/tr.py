23c23f5eee615afd7666b5fad05e166efe6abef7
SabGN <sabgn6@gmail.com>
2020-06-14 07:14:41 +0300

    development, not final version. add partial formatting

--- tr/tr.py
+++ tr/tr.py
@@ -9,6 +9,7 @@ from datetime import *
 from dateutil import tz
 
 #TODO move TIME_ZONE to main user attribute? to ClockifyClient
+#TODO get the TimeZone from Google Spreadshhet settings?
 TIME_ZONE = tz.tzoffset('MSK', 10800)  # ('Europe/Moscow') https://www.epochconverter.com/timezones
 
 #TODO move to utils
@@ -32,7 +33,6 @@ def format_timedelta(td) -> str:
     hours, minutes = divmod(minutes, 60)
     return '{:d}:{:02d}'.format(hours, minutes)
 
-#TODO refactor Setup part
 #SETUP
 CLOCKIFY_SETUP_FILE = os.path.abspath('../res/report_setup.json')
 
@@ -59,11 +59,12 @@ wks = sh.worksheet('id', REPORT_SHEET_ID)
 wks.update_value('D1', date.today().year)
 PROJECT_COLUMN = 'A'
 TASK_COLUMN = 'B'
-WEEK_FIRST_COLUMN = 'E'
+week_hours_column = 'E'
+week_money_column = 'F'
 DATA_LINE = 4
-
+model_cell = pygsheets.Cell("A1")
 # Collect data from Clockify
-weeks_in_RP = [19]
+weeks_in_RP = [9]
 week = weeks_in_RP[0]
 month = date.fromisocalendar(date.today().year, week, 1).month
 wks.update_value('E1', calendar.month_name[month])
@@ -71,13 +72,22 @@ wks.update_value('E2', week)
 projects_with_tasks = api_session.get_projects_with_tasks(workspace=WORKSPACE)
 users = api_session.get_users(workspace=WORKSPACE)
 
+cell = wks.cell('A1')
+cell.set_text_format('bold', True)
 start, end = week_start_end_datetime(week)
 time_entries = []
 for user in users:
     time_entries += api_session.get_time_entries(WORKSPACE, user, start, end)
 curr_line = DATA_LINE
+
+
+
+
 wks.unlink()
 print("Total time entries: ", len(time_entries))
+
+
+myranges=[]
 
 for project in [*projects_with_tasks]:
     proj_line = curr_line
@@ -93,21 +103,43 @@ for project in [*projects_with_tasks]:
         elapsed_timedelta = sum([time_entry.end - time_entry.start for time_entry in task_time_entries], timedelta())
 
         if elapsed_timedelta > timedelta(minutes=1):
+            #TODO measure time for that operation
             api_session.api.substitute_api_id_entities(task_time_entries, users, projects_with_tasks)
             elapsed_amount = sum([(time_entry.end - time_entry.start).seconds / 3600 *
                                   time_entry.project.get_hourly_rate(WORKSPACE, time_entry.user).amount
                                   for time_entry in task_time_entries])
         else:
             elapsed_amount = 0
-        wks.update_value(WEEK_FIRST_COLUMN + str(curr_line), format_timedelta(elapsed_timedelta))
+        wks.update_value(week_hours_column + str(curr_line), format_timedelta(elapsed_timedelta))
         wks.update_value("F" + str(curr_line), elapsed_amount)
         proj_timedelta = proj_timedelta + elapsed_timedelta
         proj_amount += elapsed_amount
         curr_line += 1
+#formatting
+    project_range = wks.range('A1:D5')
+    print(type(wks))
+    assert type(project_range) == bool
+    #project_range = wks.range(week_hours_column + str(proj_line) + ":" + week_money_column + str(proj_line))
+    #TODO Refactor using Lambda or map or generator
+    #make_bold = lambda cell: (cell.text_format['bold']:= True)
+    #map(make_bold, project_range.cells)
+    #for cell in project_range:
+    #    cell.text_format['bold'] = True
+    model_cell.set_text_format('bold', True)
+    left_corner_cell = PROJECT_COLUMN + str(proj_line)
+    right_corner_cell = week_money_column + str(proj_line)
+    myrange = pygsheets.DataRange(
+        left_corner_cell, right_corner_cell, worksheet=wks
+    )
+    myranges.append(myrange)
 
-    wks.update_value(WEEK_FIRST_COLUMN + str(proj_line), format_timedelta(proj_timedelta))
-    wks.update_value("F" + str(proj_line), elapsed_amount)
+
+
+    wks.update_value(week_hours_column + str(proj_line), format_timedelta(proj_timedelta))
+    wks.update_value(week_money_column + str(proj_line), elapsed_amount)
 
 wks.link()
-
+for my_range in myranges:
+    my_range.apply_format(model_cell)
+    my_range.update_borders(True, True, True, True, style='SOLID_THICK')
 print("==================END=====================")
